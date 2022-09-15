d9a67212f76b8906735905a199802fe648cc6544
ANS <36783010+answebdev@users.noreply.github.com>
2022-09-14 21:04:24 -0700

    add search functionality

---
+++ src/HomeWithSearch.js
@@ -0,0 +1,141 @@
+// WITH SEARCH FUNCTIONALITY
+import React, { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import './App.css';
+
+// CODE TO TEST IF DATA-RENDERING WORKS - ORIGINAL CODE BELOW (COMMENTED OUT)
+// Live test app: https://twilight-zone-api-react-test.netlify.app/
+
+// Search bar article: https://blog.logrocket.com/create-search-bar-react-from-scratch/
+
+const App = () => {
+  const [episode, setEpisodes] = useState([]);
+  // const url = `https://the-twilight-zone-api.herokuapp.com/episodes`;
+  const url = `https://the-twilight-zone-api.vercel.app/episodes`;
+
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    fetchEpisodes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function fetchEpisodes() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setEpisodes(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  return (
+    <>
+      <div style={{ padding: '20px 0' }}>
+        <h1
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Welcome to The Twilight Zone API Test Page
+        </h1>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {/* <input
+            placeholder='Search episodes'
+            onChange={(event) => setQuery(event.target.value)}
+          /> */}
+          <TextField
+            onChange={(event) => setQuery(event.target.value)}
+            id='standard-basic'
+            label='Search episodes'
+            variant='standard'
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: '#f4f4f4',
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+        }}
+      >
+        {episode
+          .filter((item) => {
+            if (query === '') {
+              return item;
+            } else if (item.title.toLowerCase().includes(query.toLowerCase())) {
+              return item;
+            }
+            // If none of the if or else-if condition matches
+            return false;
+          })
+          .map((item) => (
+            <div key={item.id} style={{ margin: '40px' }}>
+              <Card variant='outlined'>
+                <CardContent>
+                  {/* {card} */}
+                  <h3 style={{ width: '300px' }}>{item.title}</h3>
+                  <div style={{ lineHeight: '2' }}>
+                    <p style={{ width: '300px', lineHeight: '1.5' }}>
+                      <strong>Season: </strong> {item.season}
+                      <br />
+                      <strong>Episode: </strong> {item.episode}
+                      <br />
+                      <strong>Directed by: </strong>
+                      {item.directed_by}
+                      <br />
+                      <strong>Written by: </strong>
+                      {item.written_by}
+                      <br />
+                      <strong>Original Air Date: </strong>
+                      {item.air_date}
+                    </p>
+                  </div>
+                  <img
+                    style={{ width: '300px', height: '300px' }}
+                    src={item.img}
+                    alt={item.title}
+                    title={item.title}
+                  />
+
+                  <p>
+                    <a
+                      href={item.wikipedia}
+                      rel='noopener noreferrer'
+                      target='_blank'
+                    >
+                      Wikipedia
+                    </a>
+                  </p>
+                  <div>
+                    <div style={{ width: '300px' }}>
+                      <p style={{ lineHeight: '1.5' }}>
+                        <strong>Storyline: </strong>
+                        {item.storyline}
+                      </p>
+                    </div>
+                    <div style={{ lineHeight: '1.5' }}>
+                      <strong>Cast</strong>
+                    </div>
+                    {item.cast.map((c, index) => (
+                      <div key={index} style={{ lineHeight: '1.5' }}>
+                        {c}
+                      </div>
+                    ))}
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import './App.css';
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar style={{ backgroundColor: '#545454' }} position='static'>
+        <Toolbar>
+          <span
+            style={{
+              fontWeight: 'bold',
+              letterSpacing: '1.0px',
+              fontSize: '18px',
+            }}
+          >
+            The Twilight Zone API Test Page
+          </span>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
+
+// WITH A THEME: https://mui.com/material-ui/customization/palette/#adding-new-colors
+// import React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Toolbar from '@mui/material/Toolbar';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       light: '#757ce8',
+//       main: '#3f50b5',
+//       dark: '#002884',
+//       contrastText: '#fff',
+//     },
+//     secondary: {
+//       light: '#ff7961',
+//       main: '#f44336',
+//       dark: '#ba000d',
+//       contrastText: '#000',
+//     },
+//   },
+// });
+
+// // const Navbar = () => {
+// //   return (
+// //     <>
+// //       <ThemeProvider theme={theme}>
+// //         <AppBar position='static' color='secondary'>
+// //           <Toolbar>
+// //             <span
+// //               style={{
+// //                 fontWeight: 'bold',
+// //                 letterSpacing: '1.0px',
+// //                 fontSize: '18px',
+// //                 color: '#ffffff',
+// //               }}
+// //             >
+// //               The Twilight Zone API Test Page
+// //             </span>
+// //           </Toolbar>
+// //         </AppBar>
+// //         <Toolbar />
+// //       </ThemeProvider>
+// //     </>
+// //   );
+// // };
+
+// // export default Navbar;
